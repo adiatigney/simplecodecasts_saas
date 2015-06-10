2f2cca9c7e674d6bc0d093364f710733247f07fd
Adia Tigney <adiatigney@gmail.com>
2015-06-10 00:16:06 +0000

    Minor fix to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -23,6 +23,6 @@ class ContactsController < ApplicationController
   
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :body)
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
